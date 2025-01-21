5a1328b57e15a68f24e9c440e74a6ce40d85d140
rico-danielo <lemogoricdaniel@gmail.com>
2025-01-21 15:28:04 -0500

    structure de la classe joueur.h

---
+++ Joueur.h
@@ -0,0 +1,15 @@
+#include "Tour.h"
+#include <string.h>
+
+
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+class Joueur{
+
+    private:
+        string nom;
+    public:
+        Joueur();
+        ~Joueur();   
+};
