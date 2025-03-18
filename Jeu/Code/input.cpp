dc5a53604429160b2b3f6f7d72835fc79b2652bf
AtomicFrogg <pico2758@usherbrooke.ca>
2025-03-18 14:52:53 -0400

    Ajout fonction manette

--- Jeu/Code/input.cpp
+++ Jeu/Code/input.cpp
@@ -1,4 +1,8 @@
 #include "../Header/Input.h"
+static SerialPort* arduino; //doit etre un objet global!
+
+int boutonUp = 0;
+int  Direction_joystick = 0;
 
 void Input::input(GUI* gui)
 {
@@ -15,6 +19,212 @@ void Input::input(GUI* gui)
 	{
 	case '1':
 		cout << "Vous avez choisi la manette" << endl;
+		while (!FIN)
+		{
+			string raw_msg;
+
+			// Initialisation du port de communication
+			string com;
+			cout << "Entrer le port de communication du Arduino: ";
+			cin >> com;
+			arduino = new SerialPort(com.c_str(), BAUD);
+
+			//const char com = "\\\\.\\COM3";
+			//SerialPort arduino = SerialPort("\\\\.\\COM3");
+			if (!arduino->isConnected()) {
+				cerr << "Impossible de se connecter au port " << string(com) << ". Fermeture du programme!" << endl;
+				exit(1);
+			}
+
+			// Structure de donnees JSON pour envoie et reception
+			int led_state = 1;
+			int ledv_state = 1;
+			int ledy_state = 1;
+			json j_msg_send; //= "{\"ledr\",\"ledv\",\"ledy\"}";
+			json j_msg_rcv;
+
+			// Boucle pour tester la communication bidirectionnelle Arduino-PC
+			for (int i = 0; i < 500; i++) {
+				// Envoie message Arduino
+				j_msg_send["ledv"] = ledv_state;
+				j_msg_send["ledr"] = led_state;
+				j_msg_send["ledy"] = ledy_state;
+
+				if (!SendToSerial(arduino, j_msg_send)) {
+					cerr << "Erreur lors de l'envoie du message. " << endl;
+				}
+
+				// Reception message Arduino
+				j_msg_rcv.clear(); // effacer le message precedent
+				if (!RcvFromSerial(arduino, raw_msg)) {
+					cerr << "Erreur lors de la reception du message. " << endl;
+				}
+
+				// Impression du message de l'Arduino si valide
+				if (raw_msg.size() > 0) {
+					//cout << "raw_msg: " << raw_msg ;  // debug
+					// Transfert du message en json
+					j_msg_rcv = json::parse(raw_msg);
+					cout << "Message de l'Arduino: " << j_msg_rcv;
+
+					//comment assigner valeur voir ci-dessous
+					boutonUp = j_msg_rcv["boutonUp"];
+					Direction_joystick = j_msg_rcv["joystick"];
+					cout << " test_boutonUp:" << boutonUp << " test_joystick:" << Direction_joystick << "\r";
+					time = 0;
+					//Haut
+					if (Direction_joystick == 4)
+					{
+						time = 1;
+						//cout << "W" ;
+
+						gui->moveJoueurUp(1);
+
+					}
+					//Gauche key
+					if (Direction_joystick == 3)
+					{
+						time = 1;
+						//cout << "A" ;
+
+						gui->moveJoueurGauche(1);
+
+					}
+					//Bas key
+					if (Direction_joystick == 2)
+					{
+						time = 1;
+						//cout << "S" ;
+
+						gui->moveJoueurDown(1);
+
+					}
+					//Droite key
+					if (Direction_joystick == 1)
+					{
+						time = 1;
+						//cout << "D";
+
+						gui->moveJoueurDroite(1);
+
+					}
+					//Haut-Droite
+					if (Direction_joystick == 5)
+					{
+						time = 1;
+						//cout << "D";
+
+						gui->moveJoueurDroite(1);
+						gui->moveJoueurUp(1);
+
+					}
+					//Bas-Droite
+					if (Direction_joystick == 6)
+					{
+						time = 1;
+						//cout << "D";
+
+						gui->moveJoueurDroite(1);
+						gui->moveJoueurDown(1);
+
+					}
+					//Bas-Gauche
+					if (Direction_joystick == 7)
+					{
+						time = 1;
+						//cout << "D";
+
+						gui->moveJoueurGauche(1);
+						gui->moveJoueurDown(1);
+
+					}
+					//Haut-Gauche
+					if (Direction_joystick == 8)
+					{
+						time = 1;
+						//cout << "";
+
+						gui->moveJoueurGauche(1);
+						gui->moveJoueurUp(1);
+
+					}
+					//Down Button
+					if (j_msg_rcv["boutonDown"])
+					{
+						time = 1;
+						std::mutex mutex;
+						mutex.lock();
+						gui->ajouterTourBase();
+						mutex.unlock();
+					}
+					//Up Button
+					if (j_msg_rcv["boutonUp"])
+					{
+						time = 1;
+						std::mutex mutex;
+						mutex.lock();
+						gui->ajouterTourCanonnier();
+						mutex.unlock();
+					}
+					//Left Button
+					if (j_msg_rcv["boutonLeft"])
+					{
+						if (gui->getDonneesJoueur()->type >= 2)
+						{
+							time = gui->ameliorerDegat() + 2;
+						}
+						time = 1;
+						std::mutex mutex;
+						mutex.lock();
+						gui->ajouterTourSniper();
+						mutex.unlock();
+
+					}
+					//Right Button
+					if (j_msg_rcv["boutonRight"])
+					{
+						if (gui->getDonneesJoueur()->type >= 2)
+						{
+							time = gui->ameliorerRange() + 2;
+						}
+						time = 1;
+						std::mutex mutex;
+						mutex.lock();
+						gui->ajouterTourNarvolt();
+						mutex.unlock();
+					}
+					if (time >= 1)
+					{
+						Sleep(150);
+						//gui->getJoueur()->afficher(cout);
+						gui->draw();
+
+						if (time == 2)
+						{
+							cout << "Impossible d'ameliorer" << endl;
+						}
+						if (time == 3)
+						{
+							cout << "amelioration reussi" << endl;
+						}
+					}
+
+
+				}
+
+				//Changement de l'etat led
+				led_state = !led_state;
+				ledv_state = !ledv_state;
+				ledy_state = !ledy_state;
+				// Bloquer le fil pour environ 1 sec
+				Sleep(75); // 75ms
+			}
+
+
+
+
+
+		}
 
 		break;
 	case '2':
@@ -199,3 +409,47 @@ void Input::debutJeu()
 		Sleep(100);
 	}
 }
+
+
+/*---------------------------Definition de fonctions ------------------------*/
+bool SendToSerial(SerialPort* arduino, json j_msg) {
+	// Return 0 if error
+	string msg = j_msg.dump();
+	bool ret = arduino->writeSerialPort(msg.c_str(), msg.length());
+	return ret;
+}
+
+
+bool RcvFromSerial(SerialPort* arduino, string& msg) {
+	// Return 0 if error
+	// Message output in msg
+	string str_buffer;
+	char char_buffer[MSG_MAX_SIZE];
+	int buffer_size;
+
+	msg.clear(); // clear string
+	// Read serialport until '\n' character (Blocking)
+
+	// Version fonctionnel dans VScode, mais non fonctionnel avec Visual Studio
+/*
+	while(msg.back()!='\n'){
+		if(msg.size()>MSG_MAX_SIZE){
+			return false;
+		}
+
+		buffer_size = arduino->readSerialPort(char_buffer, MSG_MAX_SIZE);
+		str_buffer.assign(char_buffer, buffer_size);
+		msg.append(str_buffer);
+	}
+*/
+
+// Version fonctionnelle dans VScode et Visual Studio
+	buffer_size = arduino->readSerialPort(char_buffer, MSG_MAX_SIZE);
+	str_buffer.assign(char_buffer, buffer_size);
+	msg.append(str_buffer);
+
+	//msg.pop_back(); //remove '/n' from string
+
+	return true;
+}
+
--- Jeu/Header/Input.h
+++ Jeu/Header/Input.h
@@ -8,6 +8,24 @@
 static bool FIN = false;
 static bool debut = false;
 using namespace std;
+#include "includeJson/serial/SerialPort.hpp"
+#include "includeJson/json.hpp"
+using json = nlohmann::json;
+
+/*------------------------------ Constantes ---------------------------------*/
+#define BAUD 115200           // Frequence de transmission serielle
+#define MSG_MAX_SIZE 1024   // Longueur maximale d'un message
+
+
+/*------------------------- Prototypes de fonctions -------------------------*/
+bool SendToSerial(SerialPort* arduino, json j_msg);
+bool RcvFromSerial(SerialPort* arduino, string& msg);
+
+
+/*---------------------------- Variables globales ---------------------------*/
+
+
+
 class Input {
 public:
 
