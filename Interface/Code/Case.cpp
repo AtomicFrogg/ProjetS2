6a650468c3f90cafe436580cd3b4adc933449075
AtomicFrogg <pico2758@usherbrooke.ca>
2025-03-31 11:26:44 -0400

    ui

--- Interface/Code/Case.cpp
+++ Interface/Code/Case.cpp
@@ -8,27 +8,38 @@ Case::Case(QWidget *parent, int type, GUI* gui, int x, int y):QLabel(parent)
 	{
 
 	case 0:
-		this->setPixmap(QPixmap("Images/Grass.png").scaled(CARRE,CARRE));
+		this->setPixmap(QPixmap("Images/Grass.png").scaled(CARRE, CARRE));
 		std::cout << "0";
 		break;
 	case 1:
-		
+		if (x == 0 || x == LARGEUR - 1)
+		{
+
+		}
         if (gui->getDonnees(y + 1, x)->type == 1 && gui->getDonnees(y - 1, x)->type == 1)
         {
 			this->setPixmap(QPixmap("Images/Water2.png").scaled(CARRE, CARRE));
         }
         else if (gui->getDonnees(y,x + 1)->type == 1 && gui->getDonnees(y, x - 1)->type == 1)
         {
-			this->setPixmap(QPixmap("Images/Grass.png").scaled(CARRE, CARRE));
+			this->setPixmap(QPixmap("Images/Water.png").scaled(CARRE, CARRE));
         }
-        else if (gui->getDonnees(y, x + 1)->type == 1)
+        else if (gui->getDonnees(y, x - 1)->type == 1 && gui->getDonnees(y + 1, x)->type == 1)
         {
-			this->setPixmap(QPixmap("Images/Grass.png").scaled(CARRE, CARRE));
+			this->setPixmap(QPixmap("Images/WaterCoin.png").scaled(CARRE, CARRE));
         }
-        else if (gui->getDonnees(y, x - 1)->type == 1)
+        else if (gui->getDonnees(y, x + 1)->type == 1 && gui->getDonnees(y + 1, x)->type == 1)
         {
-			this->setPixmap(QPixmap("Images/Grass.png").scaled(CARRE, CARRE));
+			this->setPixmap(QPixmap("Images/WaterCoin1.png").scaled(CARRE, CARRE));
         }
+		else if (gui->getDonnees(y, x + 1)->type == 1 && gui->getDonnees(y - 1, x)->type == 1)
+		{
+			this->setPixmap(QPixmap("Images/WaterCoin2.png").scaled(CARRE, CARRE));
+		}
+		else if (gui->getDonnees(y, x - 1)->type == 1 && gui->getDonnees(y - 1, x)->type == 1)
+		{
+			this->setPixmap(QPixmap("Images/WaterCoin3.png").scaled(CARRE, CARRE));
+		}
 
 		std::cout << "1";
 		break;
