31b03c13f11dc9a56b5a2a2129a2d6c6217d38cb
AtomicFrogg <pico2758@usherbrooke.ca>
2025-04-01 16:22:57 -0400

    Auto stash before merge of "main" and "origin/main"

--- Interface/Header/Case.h
+++ Interface/Header/Case.h
@@ -2,14 +2,17 @@
 #include <qlabel.h>
 #include <iostream>
 #include <../../Jeu/Header/GUI.h>
+#include <qobject.h>
 const int CARRE = 30;
 class Case: public QLabel
 {
+//Q_OBJECT
 	public:
-
 		Case(QWidget* parent = nullptr, int t = 0, GUI* g = nullptr, int posi = 0, int posj = 0);
 		~Case();
 		void choixBackground(int type);
+
+	public slots:
 		void ajouterNarvolt();
 		void ajouterCanonnier();
 		void ajouterSniper();
--- Interface/Header/Interface.h
+++ Interface/Header/Interface.h
@@ -3,12 +3,15 @@
 #include <qgridlayout.h>
 #include "../../Jeu/Header/GUI.h"
 #include "Case.h"
+#include "qpushbutton.h"
+#include "qapplication.h"
 class Interface: public QWidget
 {
 public:
 	Interface(GUI *gui);
 	~Interface();
 
+	QGridLayout* getLayout();
 private:
 	QGridLayout* layout;
 };
