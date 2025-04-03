7ebe6ca19f9241b633a26d6f7e821d0643c5fb8f
AtomicFrogg <pico2758@usherbrooke.ca>
2025-04-03 12:50:00 -0400

    Auto stash before merge of "main" and "origin/main"

--- Jeu/Header/Application.h
+++ Jeu/Header/Application.h
@@ -1,10 +1,11 @@
 #include <qapplication.h>
 #include "../../Interface/Header/Interface.h"
+#include "../../Interface/Header/QtApp7.h"
 
 class Application: public QApplication
 {
 	public:
-		Application(GUI* g,int argc,char *argv[]);
+		Application(GUI* g,int argc, char *argv[]);
 		~Application();
 		Interface* getInterface();
 
