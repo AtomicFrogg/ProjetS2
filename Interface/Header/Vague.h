1cbb63dfd3dfad8fb1874ace601f9a3f6e21f24d
rico-danielo <lemogoricdaniel@gmail.com>
2025-04-08 14:16:57 -0400

    modification vague

--- Interface/Header/Vague.h
+++ Interface/Header/Vague.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "qthread.h"
 #include "qdebug.h"
+#include <iostream>
+#include <istream>
 
+using namespace std;
 class Vague:public QThread
 {
 	Q_OBJECT
+public :
+	bool lancerVague();
 protected:
 	void run() override {
 		for (int i = 0;i < 400;i++) {
